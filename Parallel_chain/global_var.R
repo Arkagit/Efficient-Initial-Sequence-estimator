b44dcaabfab24d89687d86bd73c6876b73268321
Arkagit <66682580+Arkagit@users.noreply.github.com>
2024-04-17 15:11:58 +0530

    Update
    Update

--- Parallel_chain/global_var.R
+++ Parallel_chain/global_var.R
@@ -1,26 +1,18 @@
 library(phonTools)
 library(mcmcse)
+library(multichainACF)
 
 parallel_gacf = function(par_chain, M, ch_l, chain_val){
-	chain_mean = matrix(0, nrow = M, ncol = p)
-	for(i in 1:M){
-		chain_mean[i,] = colMeans(par_chain[[i]])
-	}
-	gl_mean = colMeans(chain_mean)
-	m_diff = gl_mean - chain_mean[chain_val,]
+	
 	sigma2 = numeric(length = p)
 
 	for(i in 1:p){
+		GACF = globalACF(par_chain, chains = chain_val, component = i, type = "covariance", lag.max = ch_l - 1, plot = FALSE)
+		sigma2[i] = - GACF$chainsACF[[chain_val]][[1]][1]
 
-		acf_c = var(par_chain[[chain_val]][,i])*fastacf(par_chain[[chain_val]][,i], show = "FALSE", window = "rectangular", lag.max = floor(ch_l))$acf
-		sigma2[i] = - acf_c[1]
 		for(k in 1:floor(ch_l/2 - 1)){
-			dummy1 = acf_c[2*k - 1] - (ch_l - 2*k + 1)*m_diff[i]^(2)/ch_l - 
-		        m_diff[i]*(sum(par_chain[[chain_val]][1:(ch_l-2*k + 1),i]) + sum(par_chain[[chain_val]][(2*k):ch_l,i]) - 2*(ch_l - 2*k + 1)*gl_mean[i])/ch_l
-
-	        dummy2 = acf_c[2*k] - (ch_l - 2*k)*m_diff[i]^(2)/ch_l - 
-			    m_diff[i]*(sum(par_chain[[chain_val]][1:(ch_l-2*k),i]) + sum(par_chain[[chain_val]][(2*k+1):ch_l,i]) - 2*(ch_l - 2*k)*gl_mean[i])/ch_l
-			
+			dummy1 = GACF$chainsACF[[chain_val]][[1]][2*k - 1]
+			dummy2 = GACF$chainsACF[[chain_val]][[1]][2*k]
 			if(dummy1 + dummy2 > 0){
 				sigma2[i] = sigma2[i] + 2*(dummy1 + dummy2)
 			}else{
